ece76dfb761a71b7e54992c063141d9e95275fb9
equetzal <equetzal@huronos.org>
2023-01-20 00:07:43 -0600

    Merge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Scoreboard from './Scoreboard/Scoreboard';
 
 function App() {
+	console.log(process.env)
   return (
     <div className="AppBackground">
       <Scoreboard />
