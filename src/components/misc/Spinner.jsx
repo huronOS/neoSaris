2411e8bd3ad5fda4f0282250e82a755fe7b74561
equetzal <equetzal@huronos.org>
2023-02-05 21:48:43 -0600

    Missing sorry

--- src/components/misc/Spinner.jsx
+++ src/components/misc/Spinner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { HeartSpinner } from "react-spinners-kit";
 
+//Not the best, but is what the author recommend https://github.com/davidhu2000/react-spinners/issues/53
 const style = { position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" };
 
 const Spinner = () => {
