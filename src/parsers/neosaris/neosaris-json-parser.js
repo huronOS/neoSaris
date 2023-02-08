b3999b7edb8961906777ec9ce39a469268293218
equetzal <equetzal@huronos.org>
2023-02-07 20:28:25 -0600

    [Input][#5] Using zod for type safety of neosaris parser

--- src/parsers/neosaris/neosaris-json-parser.js
+++ src/parsers/neosaris/neosaris-json-parser.js
@@ -1,141 +1,55 @@
-const verifyObject = contestData => {
-  if (contestData == null) {
-    throw new Error("contestData is null or undefined!");
-  }
+import { z } from "zod";
 
-  //level 1 objects
-  if (contestData.contestMetadata == null) {
-    throw new Error("contestData.contestMetadata is null or undefined!");
-  }
-  if (contestData.contestants == null) {
-    throw new Error("contestData.contestants is null or undefined!");
-  }
-  if (contestData.verdictWithoutPenalty == null) {
-    throw new Error("contestData.verdictWithoutPenalty is null or undefined!");
-  }
-  if (contestData.submissions == null) {
-    throw new Error("contestData.submissions is null or undefined!");
-  }
-  //Check data types
-  if (typeof contestData.contestMetadata !== "object") {
-    throw new Error("contestData.contestMetadata is not an object!");
-  }
-  if (contestData.problems == null || !Array.isArray(contestData.problems)) {
-    throw new Error("contestData.problems is not an array!");
-  }
-  if (!Array.isArray(contestData.contestants)) {
-    throw new Error("contestData.contestants is not an array!");
-  }
-  if (typeof contestData.verdictWithoutPenalty !== "object") {
-    throw new Error("contestData.verdictWithoutPenalty is not an object!");
-  }
-  if (!Array.isArray(contestData.submissions)) {
-    throw new Error("contestData.submissions is not an array!");
-  }
-
-  //Check contestData
-  if (
-    contestData.contestMetadata.duration == null ||
-    typeof contestData.contestMetadata.duration !== "number"
-  ) {
-    throw new Error("contestData.contestMetadata.Duration is not an number!");
-  }
-  if (
-    contestData.contestMetadata.frozenTime == null ||
-    typeof contestData.contestMetadata.frozenTime !== "number"
-  ) {
-    throw new Error("contestData.contestMetadata.frozenTime is not an number!");
-  }
-  if (
-    contestData.contestMetadata.name == null ||
-    typeof contestData.contestMetadata.name !== "string"
-  ) {
-    throw new Error("contestData.contestMetadata.name is not an string!");
-  }
-  if (
-    contestData.contestMetadata.type == null ||
-    typeof contestData.contestMetadata.type !== "string" ||
-    contestData.contestMetadata.type !== "ICPC"
-  ) {
-    throw new Error('contestData.contestMetadata.type is not "ICPC"!');
-  }
-
-  //Check contestants
-  contestData.contestants.forEach(contestant => {
-    if (contestant.id == null || typeof contestant.id !== "number") {
-      throw new Error("contestData.contestant.id contain invalid data!");
-    }
-    if (contestant.name == null || typeof contestant.name !== "string" || contestant.name === "") {
-      throw new Error("contestData.contestant.name contain invalid data!");
-    }
-  });
-
-  //Check VerdictWithoutPenalty
-  for (const [key, value] of Object.entries(contestData.verdictWithoutPenalty)) {
-    if (key == null || typeof key !== "string" || key === "") {
-      throw new Error("contestData.verdictWithoutPenalty contain invalid data!");
-    }
-    if (value == null || typeof value !== "string" || value === "") {
-      throw new Error("contestData.verdictWithoutPenalty contain invalid data!");
-    }
-  }
-
-  //Check submissions
-  contestData.submissions.forEach(submission => {
-    if (submission == null) {
-      throw new Error(
-        `contestData.submissions contains invalid data!\nSubmission is null or undefined\n${JSON.stringify(
-          submission
-        )}`
-      );
-    }
-    if (submission.timeSubmission == null || typeof submission.timeSubmission !== "number") {
-      throw new Error(
-        `contestData.submissions contains invalid data!\nSubmission have invalid timeSubmission\n${JSON.stringify(
-          submission
-        )}`
-      );
-    }
-    if (
-      submission.teamName == null ||
-      typeof submission.teamName !== "string" ||
-      submission.teamName === ""
-    ) {
-      throw new Error(
-        `contestData.submissions contains invalid data!\nSubmission have invalid TeamName\n${JSON.stringify(
-          submission
-        )}`
-      );
-    }
-    if (
-      submission.problem == null ||
-      typeof submission.problem !== "string" ||
-      submission.teamName === ""
-    ) {
-      throw new Error(
-        `contestData.submissions contains invalid data!\nSubmission have invalid Problem\n${JSON.stringify(
-          submission
-        )}`
-      );
-    }
-    if (
-      submission.verdict == null ||
-      typeof submission.verdict !== "string" ||
-      submission.teamName === ""
-    ) {
-      throw new Error(
-        `contestData.submissions contains invalid data!\nSubmission have invalid verdict\n${JSON.stringify(
-          submission
-        )}`
-      );
-    }
-  });
-};
+const Data = z.object({
+  contestMetadata: z.object({
+    duration: z.number(),
+    frozenTime: z.number(),
+    name: z.string(),
+    type: z.literal("ICPC"),
+  }),
+  problems: z.array(
+    z.object({
+      index: z.string(),
+      name: z.string().optional(),
+    })
+  ),
+  contestants: z.array(
+    z.object({
+      id: z.number(),
+      name: z.string(),
+      school: z.string().optional(),
+      iconName: z.string().optional(),
+    })
+  ),
+  verdictWithoutPenalty: z.object({}).catchall(z.string()),
+  submissions: z.array(
+    z.object({
+      timeSubmission: z.number(),
+      teamName: z.string(),
+      problem: z.string(),
+      verdict: z.string(),
+    })
+  ),
+});
 
 export const getContestDataWithNeoSarisJSON = rawText => {
   let contestData = {};
   contestData = JSON.parse(rawText);
   console.log("neoSaris JSON, Input Object", contestData);
-  verifyObject(contestData);
+
+  const result = Data.safeParse(contestData);
+  if (!result.success) {
+    console.log("Zod Result", result);
+    alert(
+      result.error.issues
+        .map(issue => {
+          return `Error ${issue.code}, for ${issue.path.join(".")} expected ${issue.number}, got ${
+            issue.received
+          }`;
+        })
+        .join("\n")
+    );
+    throw new Error("Invalid neoSaris JSON");
+  }
   return contestData;
 };
---
+++ src/types/contestDataTypes.ts
@@ -0,0 +1,31 @@
+export type ContestData = {
+  contestMetadata: {
+    duration: Number; //Duration in minutes
+    frozenTime: Number; //Duration of the frozen time in minutes
+    name: String; // Title to display for the problem
+    type: "ICPC"; // Type of contest to evaluate
+    //^ We can add a future type for IOI to identify IOI contests
+  };
+  problems: Array<{
+    //0 = first problem, etc.
+    index: String; //Letter of the problem
+    name?: String; //Actual name of the problem
+    //Here we can add subtasks if we need to evaluate IOI contests
+  }>; //Array with an unique index for each problem
+  contestants: Array<{
+    id: Number; //Unique number to identify this contestant
+    name: String; //Name of the contestant
+    school?: String; //Name of represented university/school/institution
+    iconName?: String; //Id of the icon to display for this contestant
+    //For awards, we can include fields about members of the team
+  }>;
+  verdictWithoutPenalty: Map<string, string>;
+  //For awards, we can add an object for different awards (TopRanked, FirstToSolve, Medals)
+  submissions: Array<{
+    timeSubmission: Number; //Floor time in minutes of the submission, relative to the contest start.
+    teamName: String; //Name of the contestant
+    problem: String; //Should match problems array
+    verdict: String; //Should match a registered verdict
+    //For IOI we can add the partial scores
+  }>;
+};
