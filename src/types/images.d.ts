e66fc69781c212f088c0b678ad2841d39f20e9bb
Daniel Cerna (DT3264) <cernadaniel32@gmail.com>
2023-09-08 23:58:53 -0600

    Migrates Welcome to typescript

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,1 +1,11 @@
 declare module "*.png";
+declare module "*.svg" {
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
